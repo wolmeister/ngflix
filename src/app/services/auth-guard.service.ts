4c8b1640745eaa55bdc09cca4727c3297dc62076
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-01-07 22:13:49 -0300

    Implement auth guard

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AppState } from '../store/app.states';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardService implements CanActivate {
+  getState: Observable<any>;
+
+  constructor(private store: Store<AppState>, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot) {
+    return this.store
+      .select(state => state.auth.user)
+      .pipe(
+        map(user => {
+          if (route.data.isPrivate && !user) {
+            this.router.navigateByUrl('/login');
+            return false;
+          }
+          if (route.data.isAuthPage && user) {
+            this.router.navigateByUrl('/');
+            return false;
+          }
+          return true;
+        })
+      );
+  }
+}
