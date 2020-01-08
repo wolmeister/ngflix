ef77ed80630546c59949fdb33d04e7f2e61de1a9
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-01-07 23:46:26 -0300

    Implement billboard component

---
+++ src/app/components/billboard/billboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { BillboardMedia } from 'src/app/models/billboardMedia';
+
+@Component({
+  selector: 'app-billboard',
+  templateUrl: './billboard.component.html',
+  styleUrls: ['./billboard.component.scss'],
+})
+export class BillboardComponent {
+  @Input() billboard: BillboardMedia;
+  @Output() onPlay = new EventEmitter<BillboardMedia>();
+
+  handlePlay() {
+    this.onPlay.emit(this.billboard);
+  }
+}
