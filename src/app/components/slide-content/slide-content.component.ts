af7b8f6371516c145a8506639ba9fbd494d72631
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-01-09 22:39:38 -0300

    Implement slide content

---
+++ src/app/components/slide-content/slide-content.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Media } from 'src/app/models/Media';
+
+@Component({
+  selector: 'app-slide-content',
+  templateUrl: './slide-content.component.html',
+  styleUrls: ['./slide-content.component.scss'],
+})
+export class SlideContentComponent {
+  @Input() item: Media;
+  @Input() hover: boolean;
+
+  @Output() playclick = new EventEmitter<Media>();
+  @Output() infoclick = new EventEmitter<Media>();
+
+  handlePlayClick() {
+    this.playclick.emit(this.item);
+  }
+
+  handleInfoClick() {
+    this.infoclick.emit(this.item);
+  }
+}
