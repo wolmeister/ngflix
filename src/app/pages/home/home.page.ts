81cbce1188ff7cfd2c2b072b79f262a239ff2115
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-01-11 11:21:42 -0300

    Implement home page

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { BillboardService } from '../../services/billboard.service';
+import { MediaService } from '../../services/media.service';
+import { BillboardMedia } from '../../models/billboard-media';
+import { Media } from '../../models/Media';
+import { AppState } from '../../store/app.states';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePageComponent {
+  transparent = true;
+  billboard: BillboardMedia;
+  medias: Media[];
+  watchingMedias: Media[] = [];
+
+  constructor(
+    store: Store<AppState>,
+    billboardService: BillboardService,
+    mediaService: MediaService,
+    private router: Router
+  ) {
+    this.billboard = billboardService.getBillboard();
+    this.medias = mediaService.getMedias();
+    store
+      .select(state => state.auth.user)
+      .subscribe(user => {
+        this.watchingMedias = mediaService.getUnfinishedMedias(user);
+      });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.transparent = window.scrollY < 70;
+  }
+
+  handlePlay(mediaId: number) {
+    this.router.navigateByUrl(`/watch/${mediaId}`);
+  }
+}
